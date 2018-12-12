98d530fa6b91f1c6c8b45608e64c2578f50fb5f2
Marty Ballard <marty331@gmail.com>
2018-12-12 12:30:49 -0600

    update admin form to use data_required validation

--- forms/add_admin_form.py
+++ forms/add_admin_form.py
@@ -4,7 +4,7 @@ from database.admin_roles import AdminRoles
 
 
 class AddAdminForm(Form):
-    emp_id = StringField('Employee ID', [validators.required()])
+    emp_id = StringField('Employee ID', [validators.data_required()])
     name = StringField('Employee Name', )
     super_admin = BooleanField('Super Admin', default=False)
     roles = SelectMultipleField('Roles')
